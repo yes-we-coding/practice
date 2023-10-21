f32d8ab6c54c4537d6a76ef639179eaf5834978c
river <1160047023@qq.com>
2023-10-21 12:50:53 +0800

    修改选择排序方法

---
+++ src/com/bai/sort/merge/MergeSort.java
@@ -0,0 +1,21 @@
+package com.bai.sort.merge;
+
+/**
+ * 归并排序
+ */
+public class MergeSort {
+
+    public MergeSort() {
+
+    }
+
+    /**
+     * 排序方法
+     *
+     * @param arr 要排序的数组
+     * @param <E> 泛型
+     */
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+
+    }
+}
