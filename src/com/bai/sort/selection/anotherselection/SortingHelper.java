68d9a44ccff1344036c92c4c4d9a48089e8c4dc4
river <1160047023@qq.com>
2023-10-21 11:51:54 +0800

    添加排序帮助方法

--- src/com/bai/sort/selection/anotherselection/SortingHelper.java
+++ src/com/bai/sort/selection/anotherselection/SortingHelper.java
@@ -1,5 +1,7 @@
 package com.bai.sort.selection.anotherselection;
 
+import com.bai.sort.selection.SelectionSort;
+
 /**
  * 排序工具类
  */
@@ -7,5 +9,23 @@ public class SortingHelper {
   
   private SortingHelper() {}
 
-  
+  public static <E extends Comparable<E>> boolean isSorted(E[] arr) {
+
+    // 索引小的大于索引大的，返回false
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i - 1].compareTo(arr[i]) > 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public static <E extends Comparable<E>> void sortTest(String sortName, E[] arr) {
+    long startTime = System.nanoTime();
+
+    if (sortName.equals("selectionSort")) {
+      SelectionSort.sort(arr);
+    } 
+  }
 }
